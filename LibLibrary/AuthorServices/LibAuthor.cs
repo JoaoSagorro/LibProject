66c19694ae16ac80f8c6cfa2b034b68e24053b05
João Sagorro <joao.sagorro@gmail.com>
2025-02-07 12:41:20 +0000

    Created the DeleteAuthorById, GetAuthorById e CopieAuthor in the LibAuthor

--- LibLibrary/AuthorServices/LibAuthor.cs
+++ LibLibrary/AuthorServices/LibAuthor.cs
@@ -12,18 +12,79 @@ namespace LibLibrary.AuthorServices
     public class LibAuthor
     {
 
+        public static Author GetAuthorById(int authorId)
+        {
+            Author author;
+
+            try
+            {
+                using(LibraryContext context = new LibraryContext())
+                {
+                    author = context.Authors.FirstOrDefault(atr => atr.AuthorId == authorId, null);
+
+                    if (author is null) throw new Exception("Couldn't find author with the designated id."); 
+                }
+
+            } catch(Exception e)
+            {
+                throw e;
+            }
+
+            return author;
+        }
+
+
+        private static Author CopieAuthor(Author author)
+        {
+            Author newAuthor = new Author
+            {
+                AuthorId = author.AuthorId,
+                AuthorName = author.AuthorName,
+            };
+
+            return newAuthor;
+        }
+
         // Só para administradores
         // rever a função para com as verificações adequadas
         public static Author AddAuthor(Author author)
         {
-            using (LibraryContext context = new LibraryContext())
+            try
             {
-                context.Add(author);
-                context.SaveChanges();
-
-                return author;
+                using (LibraryContext context = new LibraryContext())
+                {
+                    context.Add(author);
+                    context.SaveChanges();
+                }
             }
+            catch (Exception e)
+            {
+                throw e;
+            }
+                        
+            return author;
         }
+
+        public static Author DeleteAuthorById(int id)
+        {
+            Author delAuthor;
+
+            try
+            {
+                using(LibraryContext context = new LibraryContext())
+                {
+                    Author author = GetAuthorById(id);
+                    delAuthor = CopieAuthor(author);
+                }
+            }
+            catch(Exception e)
+            {
+                throw e;
+            }
+
+            return delAuthor;
+        }
+
 
         public static bool AuthorExists(string name)
         {
