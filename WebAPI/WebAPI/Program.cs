a26b66e5e60221ee1d85a7a219be8f05b3267d0d
AlexOliveira <47390109+alex-a-oliveira@users.noreply.github.com>
2025-02-11 12:49:33 +0000

    changed Program
    deleted weatherforecast stuff

--- WebAPI/WebAPI/Program.cs
+++ WebAPI/WebAPI/Program.cs
@@ -25,27 +25,16 @@ namespace WebAPI
 
             app.UseAuthorization();
 
-            var summaries = new[]
-            {
-                "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            };
+            
 
-            app.MapGet("/weatherforecast", (HttpContext httpContext) =>
+            app.MapGet("/Login", (HttpContext httpContext) =>
             {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                    new WeatherForecast
-                    {
-                        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                        TemperatureC = Random.Shared.Next(-20, 55),
-                        Summary = summaries[Random.Shared.Next(summaries.Length)]
-                    })
-                    .ToArray();
-                return forecast;
             })
-            .WithName("GetWeatherForecast")
+            .WithName("Login")
             .WithOpenApi();
 
             app.Run();
         }
     }
 }
+
