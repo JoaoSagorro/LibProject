27c12d1ffe1888028a34b7daf55e0de346ed28da
João Sagorro <joao.sagorro@gmail.com>
2025-02-12 09:58:11 +0000

    Changed the "DeleteBookById" to receive an Id and to return the book that has been deleted.

--- LibLibrary/BookServices/LibBooks.cs
+++ LibLibrary/BookServices/LibBooks.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using EFLibrary;
 using EFLibrary.Models;
 using LibLibrary.AuthorServices;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
 namespace LibLibrary.BookServices
@@ -120,21 +121,19 @@ namespace LibLibrary.BookServices
 
         // Deleted Book
         // Still need to delete every regist of books -> when deleting book, delete from the other tables as well
+        // By default EFCore has "On delete cascade"
         public Book DeleteBookById(int bookId)
         {
             Book bookToDel = null;
             Book deleted = null;
+
             try
             {
                 using (LibraryContext context = new LibraryContext())
                 {
                     bookToDel = context.Books.FirstOrDefault(b => b.BookId == bookId, null);
-                    var bookCover = context.Covers.FirstOrDefault(bcv => bcv.CoverId == bookId);
-                    var bookCopies = context.Copies.Where(bcopie => bcopie.BookId == bookId);
-                    var bookOrders = context.Orders.Where(ord => ord.Book.BookId == bookId);
 
                     deleted = CopieBook(bookToDel);
-
                     context.Remove(bookToDel);
                     context.SaveChanges();
                 }
