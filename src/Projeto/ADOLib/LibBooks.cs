21b539e05f0e60da6de73ee2568e1ab2453f2186
João Sagorro <joao.sagorro@gmail.com>
2025-02-13 20:41:22 +0000

    Added a part of the Model and a method in LibBooks (might change the name just to Books)

---
+++ src/Projeto/ADOLib/LibBooks.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibDB;
+using Microsoft.Data.SqlClient;
+
+namespace ADOLib
+{
+    public class LibBooks
+    {
+        private string CnString { get; set; }
+
+        public LibBooks()
+        {
+            CnString = "Server=LAPTOP-DKPO5APD\\MSSQLSERVER02;Database=upskill_fake_library;Trusted_Connection=True;TrustServerCertificate=True";
+        }
+
+        public List<LibBooks> GetAllBooks()
+        {
+            DataTable dataTable = null;
+
+            List<LibBooks> books = new List<LibBooks>();
+
+            try
+            {
+                using(SqlConnection connection = new SqlConnection())
+                {
+                    string query = "SELECT * FROM Books";
+
+                    dataTable = DB.GetSQLRead(connection, query);
+
+                    foreach(DataRow row in dataTable.Rows)
+                    {
+                        var book = new LibBooks()
+                        {
+                            BookId = row["BookId"]
+                        };
+                    }
+                }
+            }
+            catch(Exception e)
+            {
+                throw new Exception(e.Message, e.InnerException);
+            }
+
+        }
+
+    }
+}
---
+++ src/Projeto/ADOLib/Model/Model.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOLib.Model
+{
+    public class Model
+    {
+
+        public class Author
+        {
+            public int AuthorId { get; set; }
+            public string AuthorName { get; set; }
+        }
+
+        public class Book
+        {
+            public int BookId { get; set; }
+            public string Title { get; set; }
+            public string Edition { get; set; }
+            public int Year { get; set; }
+            public int Quantity { get; set; }
+            public Author Author { get; set; }
+
+
+            public virtual Cover Cover { get; set; }
+            public virtual ICollection<Subject> Subjects { get; set; }
+            public virtual ICollection<Copie> Copies { get; set; }
+        }
+
+        public class Copie
+        {
+            public int BookId { get; set; }
+            public Book Book { get; set; }
+
+            public int LibraryId { get; set; }
+            public Library Library { get; set; }
+
+            public int NumberOfCopies { get; set; }
+        }
+
+        public class Cover
+        {
+            public int CoverId { get; set; }
+            public byte[] CoverImage { get; set; }
+
+            public virtual Book Book { get; set; }
+        }
+
+    }
+}
