afe8ff8902c5b51de33f38715b754ec8947b5596
AlexOliveira <47390109+alex-a-oliveira@users.noreply.github.com>
2025-02-16 19:17:38 +0000

    added managebook test

--- src/AdminMPA/AdminMPA/Pages/Admin/ManageBooks.cshtml.cs
+++ src/AdminMPA/AdminMPA/Pages/Admin/ManageBooks.cshtml.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using EFLibrary.Models;
+using LibLibrary.Services;
 
 namespace AdminMPA.Pages.Admin
 {
     public class ManageBooksModel : PageModel
     {
+        public List<Book> books { get; set; } = new();
         public void OnGet()
         {
+            books.AddRange(LibBooks.GetAllBooks());
         }
     }
 }
