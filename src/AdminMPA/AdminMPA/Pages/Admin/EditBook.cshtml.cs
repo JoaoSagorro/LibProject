3c9a2dc11df37b2a37f3b0df1d1587289d0782fc
AlexOliveira <47390109+alex-a-oliveira@users.noreply.github.com>
2025-02-17 18:19:21 +0000

    edit book page

--- src/AdminMPA/AdminMPA/Pages/Admin/EditBook.cshtml.cs
+++ src/AdminMPA/AdminMPA/Pages/Admin/EditBook.cshtml.cs
@@ -1,15 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using EFLibrary.Models;
+using LibLibrary.Services;
 
 namespace AdminMPA.Pages.Admin
 {
     public class EditBookModel : PageModel
     {
         public int BookId { get; set; }
+        public Book book { get; set; }
 
         public void OnGet(int id)
         {
-            BookId = id; // Aqui você pode buscar o livro pelo ID no banco de dados
+            BookId = id; 
+            book = LibBooks.GetBookById(id);
         }
     }
 }
