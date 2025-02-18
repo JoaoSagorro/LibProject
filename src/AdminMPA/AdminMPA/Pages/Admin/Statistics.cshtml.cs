885610a7506d618bd26f5ae26903427eb8ea615d
AlexOliveira <47390109+alex-a-oliveira@users.noreply.github.com>
2025-02-18 11:28:22 +0000

    stats page

--- src/AdminMPA/AdminMPA/Pages/Admin/Statistics.cshtml.cs
+++ src/AdminMPA/AdminMPA/Pages/Admin/Statistics.cshtml.cs
@@ -1,12 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using LibLibrary.Services;
+using EFLibrary.Models;
+using EFLibrary.ModelView;
 
 namespace AdminMPA.Pages.Admin
 {
     public class StatisticsModel : PageModel
     {
+        public List<SubjectStats> MostRequestedSubjects { get; set;}
+        public List<LibraryStats> LibsByOrders { get; set; }
         public void OnGet()
         {
+            MostRequestedSubjects = LibStatistics.GetMostRequestedSubjects();
+            LibsByOrders = LibStatistics.GetLibraryWithLessOrders();
         }
     }
 }
