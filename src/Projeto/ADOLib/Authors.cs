39fd96c1717087c37aaf1a5a0b2564e7f40981e3
João Sagorro <joao.sagorro@gmail.com>
2025-02-18 12:11:39 +0000

    Add "Authors" library to add, edit, delete authors

---
+++ src/Projeto/ADOLib/Authors.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibDB;
+using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
+using static ADOLib.Model.Model;
+
+namespace ADOLib
+{
+    public class Authors
+    {
+        private string CnString { get; set; }
+
+        public Authors()
+        {
+            CnString = "Server=LAPTOP-DKPO5APD\\MSSQLSERVER02;Database=upskill_fake_library;Trusted_Connection=True;TrustServerCertificate=True";
+        }
+
+        public Author GetAuthorById(int authorId)
+        {
+            Author author = null;
+
+            try
+            {
+                using (SqlConnection connection = DB.Open(CnString))
+                {
+                    string query = $"SELECT * FROM Authors WHERE AuthorId = {authorId}";
+                    DataTable dataTable = DB.GetSQLRead(connection, query);
+
+                    if (dataTable.Rows.Count != 1) throw new Exception("An error has occurred when trying to find the author.");
+
+                    foreach(DataRow row in dataTable.Rows)
+                    {
+                        author = new Author()
+                        {
+                            AuthorId = Convert.ToInt32(row["AuthorId"]),
+                            AuthorName = row["AuthorName"].ToString(),
+                        };
+                    }
+                }
+
+            }
+            catch (Exception e)
+
+            {
+                throw new Exception(e.Message, e.InnerException);
+            }
+
+            return author;
+        }
+
+        // Just for admins
+        public int AddAuthor(Author author)
+        {
+            try
+            {
+                // check if this verification makes sense.
+                using (SqlConnection connection = DB.Open(CnString))
+                {
+                    string query = $"INSERT INTO Books (AuthorName) " +
+                        $"VALUES ({author.AuthorName})";
+
+                    SqlTransaction transaction = connection.BeginTransaction();
+
+                    int rowsAffected = DB.CmdExecute(connection, query, transaction);
+                    transaction.Commit();
+
+                    return rowsAffected;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message, e.InnerException);
+            }
+        }
+
+        // Author parameter must contain the id of the author to be updated and the new values;
+        public int UpdateAuthor(Author author)
+        {
+            try
+            {
+                using (SqlConnection connection = DB.Open(CnString))
+                {
+                    string query = $"UPDATE Authors SET AuthorName = {author.AuthorName} WHERE AuthorId = {author.AuthorId}";
+                    SqlTransaction transaction = connection.BeginTransaction();
+
+                    int rowsAffected = DB.CmdExecute(connection, query, transaction);
+
+                    transaction.Commit();
+                    return rowsAffected;
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message, e.InnerException);
+            }
+        }
+
+        public static Author DeleteAuthorById(int id)
+        {
+            Author delAuthor;
+
+            try
+            {
+
+                using (LibraryContext context = new LibraryContext())
+                {
+                    Author author = GetAuthorById(id);
+                    delAuthor = CopieAuthor(author);
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message, e.InnerException);
+            }
+
+            return delAuthor;
+        }
+
+
+
+        public static bool AuthorExists(string name)
+        {
+            using (LibraryContext context = new LibraryContext())
+            {
+                List<Author> authors = context.Authors.Where(a => a.AuthorName == name).ToList();
+
+                return !authors.IsNullOrEmpty();
+            }
+        }
+
+    }
+
+}
+}
