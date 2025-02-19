32c6d2ab025644b6efaf999a327635eed97eb139
João Sagorro <joao.sagorro@gmail.com>
2025-02-19 22:01:42 +0000

    Add a comment to books!

--- src/Projeto/ADOLib/Books.cs
+++ src/Projeto/ADOLib/Books.cs
@@ -21,6 +21,7 @@ namespace ADOLib
             CnString = "Server=LAPTOP-DKPO5APD\\MSSQLSERVER02;Database=upskill_fake_library;Trusted_Connection=True;TrustServerCertificate=True";
         }
 
+        // Make a method called CreateBooksInfo where it gathers all the info from all the books and returns a list of it.
 
         private string QueryCreator(int? id = null)
         {
