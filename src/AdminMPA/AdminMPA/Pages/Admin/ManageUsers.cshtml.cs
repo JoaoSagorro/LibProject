f08e3de2065bf3c8de5901f78ec59b04bb88cc6e
João Sagorro <joao.sagorro@gmail.com>
2025-02-22 12:22:16 +0000

    Change navebar, and ManageUsers page. Just the frontend. Did nothing on the logic behind it

--- src/AdminMPA/AdminMPA/Pages/Admin/ManageUsers.cshtml.cs
+++ src/AdminMPA/AdminMPA/Pages/Admin/ManageUsers.cshtml.cs
@@ -2,23 +2,30 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using LibLibrary.Services;
 using EFLibrary.Models;
+using static System.Formats.Asn1.AsnWriter;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+using System.Collections.Generic;
 
 namespace AdminMPA.Pages.Admin
 {
     public class ManageUsersModel : PageModel
     {
         public List<User> deletedUsers { get; set; } = [];
+        public int Number { get; set; }
         public List<User> Users { get; set; } = [];
+        public List<User> AllUsers { get; set; } = [];
+
         [BindProperty(SupportsGet = true)]
         public string SearchTerm { get; set; }
         public IActionResult OnGet()
         {
+            Number = 0;
             if (HttpContext.Session.GetString("User") != null)
             {
-                var query = LibUser.GetUsers();
+                AllUsers = LibUser.GetUsers();
                 if (!string.IsNullOrEmpty(SearchTerm))
                 {
-                    Users = query.Where(u => u.Email.ToLower().Contains(SearchTerm.ToLower()) || u.FirstName.ToLower().Contains(SearchTerm.ToLower()) || u.LastName.ToLower().Contains(SearchTerm.ToLower())).ToList();
+                    Users = AllUsers.Where(u => u.Email.ToLower().Contains(SearchTerm.ToLower()) || u.FirstName.ToLower().Contains(SearchTerm.ToLower()) || u.LastName.ToLower().Contains(SearchTerm.ToLower())).ToList();
                     //Users = query.ToList();
                 }
                 return Page();
