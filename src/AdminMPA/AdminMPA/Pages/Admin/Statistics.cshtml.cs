d0a336c6f7b5d5ef1783dd9c7e7ee90223edbf06
João Sagorro <joao.sagorro@gmail.com>
2025-02-25 16:48:49 +0000

    Changes in CSS and HTML

--- src/AdminMPA/AdminMPA/Pages/Admin/Statistics.cshtml.cs
+++ src/AdminMPA/AdminMPA/Pages/Admin/Statistics.cshtml.cs
@@ -3,19 +3,31 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using LibLibrary.Services;
 using EFLibrary.Models;
 using EFLibrary.ModelView;
+using Microsoft.Win32;
+using static System.Formats.Asn1.AsnWriter;
+using System.Threading;
+using System;
 
 namespace AdminMPA.Pages.Admin
 {
     public class StatisticsModel : PageModel
     {
         public List<SubjectStats> MostRequestedSubjects { get; set;}
+        public int RequestedNumber { get; set; } = 0;
         public List<LibraryStats> LibsByOrders { get; set; }
+        public int OrdersNumber { get; set; } = 0;
+
+        public List<LibraryStats> LibsByOldestRequest { get; set; }
+        public int OldestNumber { get; set; } = 0;
+
+
         public IActionResult OnGet()
         {
             if (HttpContext.Session.GetString("User") != null)
             {
                 MostRequestedSubjects = LibStatistics.GetMostRequestedSubjects();
                 LibsByOrders = LibStatistics.GetLibraryWithLessOrders();
+                LibsByOldestRequest = LibStatistics.GetLibraryWithOldestRequest();
 
                 return Page();
             }
