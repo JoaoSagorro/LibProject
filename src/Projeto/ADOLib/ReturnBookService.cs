fd441b73644434cb9ab6d0bec39a0c0107433420
AlexOliveira <47390109+alex-a-oliveira@users.noreply.github.com>
2025-03-03 23:43:44 +0000

    awwh shit

--- src/Projeto/ADOLib/ReturnBookService.cs
+++ src/Projeto/ADOLib/ReturnBookService.cs
@@ -95,7 +95,7 @@ namespace ADOLib
                 if ((DateTime.UtcNow - date).Days > 15)
                 {
                     var user = new Users();
-                    int strikes = user.StrikeUser(orderId, transaction);
+                    int strikes = user.StrikeUser(orderId, transaction, connection);
                     if (strikes > 3)
                     {
                         string suspendQuery = $@"UPDATE u
@@ -198,7 +198,7 @@ namespace ADOLib
                 if ((DateTime.UtcNow - date).Days > 15)
                 {
                     var user = new Users();
-                    int strikes = user.StrikeUser(orderId, transaction);
+                    int strikes = user.StrikeUser(orderId, transaction, connection);
                     if (strikes > 3)
                     {
                         string suspendQuery = $@"UPDATE u
